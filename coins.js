e19372f907f7db6700e20b2dea647384adcaffc7
Joshua Meyers <joshua.r.meyers@gmail.com>
2017-07-02 10:16:32 -0500

    changed conlogs to right thing, removed inputs and just used a variable for coins. works

--- coins.js
+++ coins.js
@@ -11,9 +11,10 @@
 */
 
 //dom elements
-var coins = document.getElementById("coins");
-var button = document.getElementById("button").addEventListener("click", coinCounter);
-var output = document.getElementById("output");
+// var coins = document.getElementById("coins");
+var coins = (5.43);
+// var button = document.getElementById("button").addEventListener("click", coinCounter);
+// var output = document.getElementById("output");
 
 // values
 let quarterVal = .25;
@@ -21,31 +22,30 @@ let nickelVal = .05;
 let dimeVal = .1;
 let pennyVal = .01;
 
-function coinCounter (coins) {
+function coinCounter () {
   // Initialize a JavaScript object to hold the coins
   var coinPurse = {};
   if (coins >= quarterVal) {
 	coinPurse.quarters = Math.floor(coins / quarterVal);
 	coins = (coins % quarterVal);  	
+	  console.log("quarters", coinPurse.quarters);
   }
-  console.log("coins", coins);
   if (coins >= dimeVal) {
 	coinPurse.dimes = Math.floor(coins / dimeVal);
   	coins = (coins % dimeVal);
+	  console.log("dimes", coinPurse.dimes);
   }
-  console.log("coin", coins);
   if (coins >= nickelVal) {
 	coinPurse.nickels = Math.floor(coins / nickelVal);
   	coins = (coins % nickelVal);
+	  console.log("nickels", coinPurse.nickels);
   }
-  console.log("coins", coins);
   if (coins >= pennyVal) {
 	coinPurse.pennies = Math.floor(coins / pennyVal);
+	console.log("pennies", coinPurse.pennies);
   }
-
-
   return coinPurse;
 }
 
-var coinage = coinCounter(coins)
-console.log(coinage);
+
+console.log(coinCounter());
